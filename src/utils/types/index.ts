4c12299542c8c123f5def360797c1ebcc8846d84
Danny944 <fadehandaniel2006@gmail.com>
2025-07-04 17:43:45 +0100

    version update

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,4 +1,4 @@
-import { Prisma, OrderStatus } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
 declare global {
   namespace Express {
@@ -13,17 +13,6 @@ export interface ILoginBody {
   password: string;
 }
 
-export type ICreateProduct = Prisma.ProductCreateInput & { categoryId: string };
-export type IUpdateProduct = Prisma.ProductUpdateInput & { categoryId: string };
-export type ICreateOrder = Prisma.OrderCreateInput & {
-  userId: string;
-  items: {
-    productId: string;
-    quantity: number;
-    price: number;
-  }[];
-};
-
 export type TokenPayload = Omit<Prisma.UserCreateInput, "password">;
 
-export type IOrderStatus = OrderStatus;
+
